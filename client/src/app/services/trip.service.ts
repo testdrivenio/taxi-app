cfea310e16e09a9707586d8406e08011e1356804
Jason Parent <jason.a.parent@gmail.com>
2019-03-09 00:17:30 -0500

    part-3-chapter-4

--- client/src/app/services/trip.service.ts
+++ client/src/app/services/trip.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { WebSocketSubject } from 'rxjs/webSocket';
+import { map, share } from 'rxjs/operators';
 
 import { User } from './auth.service';
 
 export class Trip {
   constructor(
-    public id?: string,
+    public id?: number,
+    public nk?: string,
     public created?: string,
     public updated?: string,
     public pick_up_address?: string,
@@ -21,6 +23,7 @@ export class Trip {
   static create(data: any): Trip {
     return new Trip(
       data.id,
+      data.nk,
       data.created,
       data.updated,
       data.pick_up_address,
@@ -36,13 +39,34 @@ export class Trip {
   providedIn: 'root'
 })
 export class TripService {
+
+  webSocket: WebSocketSubject<any>;
+  messages: Observable<any>;
+
   constructor(
     private http: HttpClient
   ) {}
 
+  connect(): void {
+    if (!this.webSocket || this.webSocket.closed) {
+      this.webSocket = new WebSocketSubject('ws://localhost:8080/taxi/');
+      this.messages = this.webSocket.pipe(share());
+      this.messages.subscribe(message => console.log(message));
+    }
+  }
   getTrips(): Observable<Trip[]> {
     return this.http.get<Trip[]>('/api/trip/').pipe(
       map(trips => trips.map(trip => Trip.create(trip)))
     );
+  }
+  createTrip(trip: Trip): void {
+    this.connect();
+    const message: any = {
+      type: 'create.trip',
+      data: {
+        ...trip, rider: trip.rider.id
+      }
+    };
+    this.webSocket.next(message);
   }
 }
